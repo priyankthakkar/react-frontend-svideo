4f362386b7741940ffd9c7a1013ef991601dab28
Priyank Thakkar <priyankthakkar@gmail.com>
2017-07-19 23:41:31 +1000

    Change Log: 1. updated JEST test cases after adding redux. Changes By: Priyank Thakkar(priyankthakkar@gmail.com)

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -4,22 +4,22 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import preload from '../../data.json';
 import ShowCard from '../ShowCard';
-import Search from '../Search';
+import Search, { Unwrapped as UnwrappedSearch } from '../Search';
 
 test('Search rnders correctly', () => {
-	const component = shallow(<Search shows={preload.shows} />);
+	const component = shallow(<UnwrappedSearch shows={preload.shows} searchTerm="" />);
 	expect(component).toMatchSnapshot();
 });
 
 test('Search should show correct amount of ShowCard renders', () => {
-	const component = shallow(<Search shows={preload.shows} />);
+	const component = shallow(<UnwrappedSearch shows={preload.shows} searchTerm="" />);
 	expect(component.find(ShowCard).length).toEqual(preload.shows.length);
 });
 
 test('Search should show correct of ShowCard renders based on SearchTerm', () => {
 	const searchWord = 'black';
-	const component = shallow(<Search shows={preload.shows} />);
-	component.find('input').simulate('change', { target: { value: searchWord } });
+	const component = shallow(<UnwrappedSearch shows={preload.shows} searchTerm={searchWord} />);
+	/* component.find('input').simulate('change', { target: { value: searchWord } }); */
 	const showCount = preload.shows.filter(
 		show => `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0
 	).length;
